44423e1e09588ed16d72f0b12fc4701c1e06f299
rahul ansari <rahulanari3738@gmail.com>
2022-01-12 10:22:45 +0530

    update signUpControlle

--- controller/signUpController.js
+++ controller/signUpController.js
@@ -5,20 +5,19 @@ const tokenGenerator=require("../middleware/genjwt")
 class userController {
   signUp = async (req, res) => {
     try {
-      const {firstName, lastName, emailId,   password, reEnterpassword, Hint } =req.body;
-
+      const {firstName,lastName,emailId,password,reEnterPassword,Hint } =req.body;
+    
         console.log(req.body)
      
-      if (!firstName||!lastName||!emailId||!  password||!reEnterpassword||!Hint)
+      if (!firstName || !lastName|| !emailId || !password || !reEnterPassword|| !Hint)
   
      {
-       
         return res.status(206).json({ message: "please fill the field", success: false });
       }
 
-      if(  password!=reEnterpassword)
+      if( password!=reEnterPassword)
       {
-          return res.status(400).json({ message: "please enter the same   password", success: false})
+          return res.status(400).json({ message: "please enter the same  password", success: false})
       }
 
       const userexits = await collection.findOne({ emailId: emailId });
@@ -26,45 +25,31 @@ class userController {
         return res .status(400).json({ message: "User Allready Exist", success: false });     
       }
       
-      
-      
-      else {
-        const adding = new collection({ firstName, lastName,  emailId,   password, reEnterpassword, Hint,
-         
-        });
-        
-    
-        const result = await adding.save();
-        return res
-          .status(200)
-          .json({
-            message: "user successfully register",
-            success: true,
-            result,
-          });
+        else {
+          const adding = new collection({firstName,lastName,emailId,password,reEnterPassword,Hint});
+          const result = await adding.save();
+          return res.status(200).json({message: "user successfully register", success: true,result});
       }
     } catch (e) {
       console.log(e);
-      return res
-        .status(400)
-        .json(e, { message: "server error", success: false });
+      return res.status(500).json(e,{ message: "server error", success: false });
     }
   };
 
   login = async (req, res) => {
 
     try {
-        const {emailId,   password } = req.body; 
+        const {emailId,password } = req.body; 
        
         console.log(req.body);
-        if(!emailId || !  password)
+        if(!emailId || !password)
         {
            return res.status(206).json({"message":"Please fill the Field","success":false})
         }
-        const user = await collection.findOne({emailId })
+        const user = await collection.findOne({emailId:emailId})
         if (!user) return res.status(404).json({ "message": "not found","success":false });
 
-        if (user.  password !=   password) {
+        if (user.password!=password) {
             return res.json({ "message": "Invalid   password" })
         }
 
@@ -79,8 +64,6 @@ class userController {
         console.log(e)
         return res.status(500).json({ "message": "server error","success":false })
     }
-
-
   };
 
 }
